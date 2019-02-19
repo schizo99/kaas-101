bf220f42e9b6361bd7fea41abf8cd34599d8b76f
Andreas Åhman <andreas.ahman@ikea.com>
2019-02-19 10:24:56 +0100

    Add /heatlh

--- app/app.py
+++ app/app.py
@@ -3,11 +3,15 @@ import os
 import socket
 
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def hello():
     html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+
+@app.route("/health")
+def health():
+    return "ok"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000)
